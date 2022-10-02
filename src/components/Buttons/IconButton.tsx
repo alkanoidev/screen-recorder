fdb60ca5ce8df40e12408c500bfc62dbbd7900cc
Filip Ivanovic <alkanoidev@gmail.com>
2022-10-02 22:25:20 +0200

    buttons styles

--- src/components/Buttons/IconButton.tsx
+++ src/components/Buttons/IconButton.tsx
@@ -5,7 +5,7 @@ export default function IconButton({ children, onClick, className }: Props) {
     <button
       onClick={onClick}
       className={
-        "rounded-full bg-light px-2 py-2 flex items-center justify-center gap-2 font-bold hover:scale-105 transition" +
+        "rounded-full bg-light px-2 py-2 flex items-center justify-center gap-2 font-bold hover:ring hover:ring-light/50 transition-all" +
         className
       }
     >
